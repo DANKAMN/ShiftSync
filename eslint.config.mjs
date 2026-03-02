09070afd8bdfbdad8909aed57c526039bd5dfe9b
Daniel Nicholas <danielnicholas1477@gmail.com>
2026-03-02 21:42:17 +0100

    new installations and linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,20 @@
 // const eslintConfig = defineConfig([
 //   ...nextVitals,
 //   ...nextTs,
+//   {
+//     // This section targets your files and turns off the annoying rules
+//     rules: {
+//       "@typescript-eslint/no-explicit-any": "off",
+//       "@typescript-eslint/no-unused-vars": "off",
+//       "@typescript-eslint/no-unsafe-assignment": "off",
+//       "@typescript-eslint/no-unsafe-member-access": "off",
+//       "@typescript-eslint/no-unsafe-argument": "off",
+//       "@typescript-eslint/no-explicit-any": "off",
+//       "react/no-unescaped-entities": "off",
+//     },
+//   },
 //   // Override default ignores of eslint-config-next.
 //   globalIgnores([
-//     // Default ignores of eslint-config-next:
 //     ".next/**",
 //     "out/**",
 //     "build/**",
@@ -32,8 +43,9 @@ const eslintConfig = defineConfig([
       "@typescript-eslint/no-unsafe-assignment": "off",
       "@typescript-eslint/no-unsafe-member-access": "off",
       "@typescript-eslint/no-unsafe-argument": "off",
-      "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities": "off",
+      // --- ADD THIS LINE BELOW ---
+      "@typescript-eslint/no-this-alias": "off",
     },
   },
   // Override default ignores of eslint-config-next.
